be6dd3b52efb32ebfb2a05dd388c9a1a20903476
shinjuho2 <sinjuho2@gmail.com>
2024-01-02 12:08:58 +0900

    수정함

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -6,13 +6,14 @@ import style from './myComponent.module.css';
 
 
 function MyComponent() {
+    var [value, setValue] = useState('')
     return <div className={style.myComponent}>
         <header className={style.myComponentHeader}>
-            <Navbar />
+            <Navbar onChange={(value) => setValue(value)}/>
         </header>
         <div className={style.myComponentBody}>
             <h1>내 프로필을 검색해보자!</h1>
-            <MyProfile />
+            {value === "신주호" ? <MyProfile/> : value === '' ? <p>상단 검색바에서 자신의 이름을 검색해보세요!~</p> : <p>찾을 수 없습니다.</p>}
         </div>
     </div>;
 }
--- src/MyProfile.js
+++ src/MyProfile.js
@@ -2,23 +2,23 @@ import style from "./myProfile.module.css";
 
 function MyProfile() {
     return <div className={style.profileContiner}>
-        <img src={`${process.env.PUBLIC_URL}/profile_image.jpg`} />
+        <img src={`${process.env.PUBLIC_URL}/고양ㅇ.jpg`} />
         <ul className={style.profileInfo}>
             <li className={style.profileRow}>
                 <h5>이름</h5>
-                <p>정택유</p>
+                <p>신주호</p>
             </li>
             <li className={style.profileRow}>
                 <h5>나이</h5>
-                <p>27</p>
+                <p>18</p>
             </li>
             <li className={style.profileRow}>
                 <h5>직업</h5>
-                <p>프톤트개발자</p>
+                <p>학생</p>
             </li>
             <li className={style.profileRow}>
                 <h5>직장</h5>
-                <p>투네이션</p>
+                <p>한세사이버보안고등학교</p>
             </li>
         </ul>
     </div>;
